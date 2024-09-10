6cc3bbb95a7b75d7b727f1440ffe74d4a98540fc
felipemelo7782 <dg203253@gmail.com>
2024-09-10 14:10:28 -0300

    10/09/2024

---
+++ python/exercicio10.py
@@ -0,0 +1,28 @@
+num = int(input(f"Insira o Mês \n -----VALOR EM NUMEROS-----\n------ DE 1 A 12------\n"))
+
+if num == 1:
+    print("Janeiro")
+elif num == 2:
+    print("Fevereiro")
+elif num == 3:
+    print("Março")
+elif num == 4:
+    print("Abril")
+elif num == 5:
+    print("Maio")
+elif num == 6:
+    print("Junho")
+elif num == 7:
+    print("Julho")
+elif num == 8:
+    print("Agosto")
+elif num == 9:
+    print("Setembro")
+elif num == 10:
+    print("Outubro")
+elif num == 11:
+    print("Novembro")
+elif num == 12:
+    print("Dezembro")
+else :
+    print(f"Mês Inválido")
