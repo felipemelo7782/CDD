ee9a40aeec13a60554fefbe0d1806af9c0c8dfc4
felipemelo7782 <dg203253@gmail.com>
2024-09-10 16:57:51 -0300

    10/09/2024 - pos intervalo

--- python/exercicio16.py
+++ python/exercicio16.py
@@ -1,8 +1,13 @@
-num = int(input("Digite o numero de alunos : "))
-
+#RECEBE QUANTIDADE DE ALUNOS NA VARIAVEL alunos
+alunos = int(input("Digite o numero de alunos : "))
+#INICIA A VARIAVEL GLOBALMENTE
 somaNotas=0
-for aluno in range(1,num+1):
+#INICIA O FOR COM A QUANTIDADE DE ALUNOS+1 (POIS ELE SO VAI ATE A QUANTIDADE)E INICIA COM 1
+for aluno in range(1, alunos + 1):
+    #RECEBE A NOTA DO ALUNO ATUAL EM FLOAT
     nota = float(input(f"Digite a nota do aluno {aluno}: "))
+    #SOMA O TOTAL DAS NOTAS
     somaNotas= somaNotas+nota
-media=somaNotas/num
+
+media= somaNotas / alunos
 print(f"MEDIA ARITMETICA DA TURMA: {media}")
