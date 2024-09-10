977c8fe68569366b278fadb34421c18918731c7f
felipemelo7782 <dg203253@gmail.com>
2024-09-10 14:21:31 -0300

    10/09/2024

--- python/exercicio10.py
+++ python/exercicio10.py
@@ -1,6 +1,8 @@
 num = int(input(f"Insira o Mês \n -----VALOR EM NUMEROS-----\n------ DE 1 A 12------\n"))
 
-if num == 1:
+if num >12 or num<1:
+    print(f"Mês Inválido")
+elif num == 1:
     print("Janeiro")
 elif num == 2:
     print("Fevereiro")
@@ -24,5 +26,33 @@ elif num == 11:
     print("Novembro")
 elif num == 12:
     print("Dezembro")
-else :
-    print(f"Mês Inválido")
+
+# OUTRO METODO
+# match num:
+#
+#     case 1:
+#         print("Janeiro")
+#     case 2:
+#         print("Fevereiro")
+#     case 3:
+#         print("Março")
+#     case 4:
+#         print("Abril")
+#     case 5:
+#         print("Maio")
+#     case 6:
+#         print("Junho")
+#     case 7:
+#         print("Julho")
+#     case 8:
+#         print("Agosto")
+#     case 9:
+#         print("Setembro")
+#     case 10:
+#         print("Outubro")
+#     case 11:
+#         print("Novembro")
+#     case 12:
+#         print("Dezembro")
+#     case _:
+#         print(f"Mês Inválido")
